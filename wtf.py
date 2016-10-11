01d864c42a05a0661c2893f19f2f1d4008527ccd
Éder Alcazar <ederalcazar@gmail.com>
2016-10-11 15:12:28 -0300

    Corrigido erro um

--- wtf.py
+++ wtf.py
@@ -6,6 +6,7 @@ import time
 import threading
 import Queue
 import sets
+import json
 #***DONT TOUCH HERE
 from pyspark import SparkConf, SparkContext
 from datetime import datetime,tzinfo
@@ -24,7 +25,7 @@ CREATION_TIME = datetime.utcnow()
 #DEFINE APP_NAME
 APP_NAME = "Wiki to File"
 #DEFINE MASTER_URL
-MASTER_URL = TEST_URL
+MASTER_URL = LOCAL_URL
 #DEFINE OVERWRITE_STD
 OVERWRITE_STD=True
 #DEFINE FILE_STDOUT
@@ -35,35 +36,40 @@ FILE_STDERR = "py_err.txt"
 
 
 #DEFINE MULTITHREAD
-PAGE_LIMIT = 256
+PAGE_LIMIT = 1024
 MULTITHREAD_LIMIT = 8
 FLUSH_IO_BATCH = 32
-PAGE_LIMIT_SEMAPHORE = threading.Semaphore()
+#PAGE_LIMIT_SEMAPHORE = threading.Semaphore()
 READY_SET = sets.Set()
 ALL_SET = sets.Set()
 SET_LOCK = threading.RLock()
-MAIN_SEMAPHORE = threading.Semaphore()
+IO_LOCK = threading.RLock()
+#MAIN_SEMAPHORE = threading.Semaphore(PAGE_LIMIT)
 
 #Main Code:
 def main(sc):
-    languages = {"en","pt","fr"}
+    languages = {"en","de"}
+    initial = threading.activeCount()
+    wikipedia.set_rate_limiting(True)
     for language in languages:
         WIKI_DICT = {}
         try:
-            MAIN_SEMAPHORE = threading.Semaphore(MULTITHREAD_LIMIT)
-            PAGE_LIMIT_SEMAPHORE = threading.Semaphore(PAGE_LIMIT)
+            print "== "+language+" =="
+            #MAIN_SEMAPHORE = threading.Semaphore(MULTITHREAD_LIMIT)
+            #PAGE_LIMIT_SEMAPHORE = threading.Semaphore(PAGE_LIMIT)
             wikipedia.set_lang(language)
             flang=open("wikipedia_"+language+".json","a")
             randPageNames = wikipedia.random(pages=10)
             ALL_SET.update(randPageNames)
+            print ALL_SET
             READY_SET.update(ALL_SET)
             for i in range(1,MULTITHREAD_LIMIT):
-                thread.start_new_thread(getPages,(flang,))
+                thread.start_new_thread(getPages,(flang,PAGE_LIMIT/MULTITHREAD_LIMIT))
                 pass
             time.sleep(5)
-            for i in range(1,MULTITHREAD_LIMIT):
-                MAIN_SEMAPHORE.acquire()
-                pass
+            #for i in range(1,MULTITHREAD_LIMIT):
+                #MAIN_SEMAPHORE.acquire()
+                #pass
         except wikipedia.exceptions.DisambiguationError as e:
             print >> sys.stderr, e
         except wikipedia.exceptions.PageError as e:
@@ -75,30 +81,47 @@ def main(sc):
         except Exception as e:
             print >> sys.stderr, e
         pass
-    return
+    for i in range (1,PAGE_LIMIT):
+        if threading.activeCount()!=initial:
+            time.sleep(1)
+            print "Waiting(%ds)" % (i)
+        else:
+            return
+        pass
 
-def getPages(flang):
-    MAIN_SEMAPHORE.acquire()
+    #return
+
+def getPages(flang,limit=1):
+    #MAIN_SEMAPHORE.acquire()
     #Creates a local dictionary of the pages acquired and per requests
     wikidict = {}
-    i=0
     #Try to acquire a page
-    while PAGE_LIMIT_SEMAPHORE.acquire(False):
-        try:
-            #Get a request and execute it
-            addPage(wikidict)
-            i+=1
-            if i%FLUSH_IO_BATCH == 0:
-                thread.start_new_thread(flushIO,(wikidict,flang,))
-                wikidict = {}
-        except Exception as e:
-            print >> sys.stderr, e
+    i=0
+    while addPage(wikidict)
+        i +=1
+        if i%FLUSH_IO_BATCH == 0:
+            thread.start_new_thread(flushIO,(wikidict,flang,))
+            wikidict = {}
+        pass
+#    for i in range(1,limit):
+#    #while PAGE_LIMIT_SEMAPHORE.acquire(False):
+#        try:
+#            #Get a request and execute it
+#            wikidict=addPage(wikidict)
+#            if i%FLUSH_IO_BATCH == 0:
+#                thread.start_new_thread(flushIO,(wikidict,flang,))
+#                wikidict = {}
+#        except Exception as e:
+#            print >> sys.stderr, e
+#        pass
     flushIO(wikidict,flang)
-    MAIN_SEMAPHORE.release()
+    #print "Feitos=%d"% (i)
+    #MAIN_SEMAPHORE.release()
 
 def flushIO(wikidict,flang=sys.stdout):
     try:
-        print >> flang, wikidict
+        with IO_LOCK:
+            print >> flang, json.dumps(wikidict, sort_keys=True,indent=4)
     except Exception as e:
         print >> sys.stderr, e
 
@@ -111,16 +134,23 @@ def queuePages(pageNames):
         pass
 
 
-def addPage(wikidict={},pageName =""):
-    pageName=READY_SET.pop()
+def addPage(wikidict={}):
+    try:
+        with SET_LOCK:
+            pageName=READY_SET.pop()
+        print pageName
+    except Exception as e:
+        print >> sys.stderr, e
+        return False
     try:
         page = wikipedia.page(pageName)
         links = page.links
         pagedict = {"pageName":pageName,"content":page.content,"links":links,"images":page.images}
+        #print pagedict
         wikidict[pageName]=pagedict
         queuePages(links)
     except wikipedia.exceptions.DisambiguationError as e:
-        #Common Error
+        print >> sys.stderr, e
         queuePages(e.options)
     except wikipedia.exceptions.PageError as e:
         print >> sys.stderr, e
@@ -128,7 +158,7 @@ def addPage(wikidict={},pageName =""):
         print >> sys.stderr, e
     except wikipedia.exceptions.WikipediaException as e:
         print >> sys.stderr, e
-    return wikidict
+    return True
 
 #DONT TOUCH HERE
 if __name__ == "__main__":
